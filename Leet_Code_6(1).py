0201170129bcaa424463d5518767d0e60aeb96e1
SnehaAdki <adkisneha1201@gmail.com>
2025-08-22 20:00:22 +0530

    Update and rename Problem_6.py to Leet_Code_6(1).py

--- Leet_Code_6(1).py
+++ Leet_Code_6(1).py
@@ -1,3 +1,24 @@
+# DP (Solution_1)
+# https://leetcode.com/problems/longest-palindromic-substring/description/
+# 5. Longest Palindromic Substring
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Given a string s, return the longest palindromic substring in s.
+
+ 
+# Example 1:
+# Input: s = "babad"
+# Output: "bab"
+# Explanation: "aba" is also a valid answer.
+
+# Example 2:
+# Input: s = "cbbd"
+# Output: "bb"
+
 def lon_sub_sring(string):
     if len(string) <= 1:
         return string
