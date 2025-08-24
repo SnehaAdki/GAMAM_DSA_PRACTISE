8b69f83c824b9d3555b4685c0d910129853ca976
SnehaAdki <adkisneha1201@gmail.com>
2025-08-24 23:14:08 +0530

    Create Leet_Code_287.py

---
+++ Leet_Code_287.py
@@ -0,0 +1,39 @@
+# DP
+# 287. 
+# Find the Duplicate Number
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+# There is only one repeated number in nums, return this repeated number.
+
+# You must solve the problem without modifying the array nums and using only constant extra space.
+
+ 
+
+# Example 1:
+# Input: nums = [1,3,4,2,2]
+# Output: 2
+
+# Example 2:
+# Input: nums = [3,1,3,4,2]
+# Output: 3
+
+# Example 3:
+# Input: nums = [3,3,3,3,3]
+# Output: 3
+
+def findDuplicate(nums):
+    hash_map = {}
+    for i in nums:
+        if i in hash_map:
+            return i
+        hash_map[i] = 1
+
+
+nums = [1,3,4,2,2]
+print(findDuplicate(nums))
+  
