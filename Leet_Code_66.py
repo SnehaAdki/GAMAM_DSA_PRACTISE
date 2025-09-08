894ad1541b3c1a2ed959fff3cf8e2092af766881
SnehaAdki <adkisneha1201@gmail.com>
2025-09-08 20:14:22 +0530

    Create Leet_Code_66.py

---
+++ Leet_Code_66.py
@@ -0,0 +1,42 @@
+# 66. Plus One
+# https://leetcode.com/problems/plus-one/submissions/1763882486/
+
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+
+# Example 1:
+# Input: digits = [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+# Incrementing by one gives 123 + 1 = 124.
+# Thus, the result should be [1,2,4].
+
+# Example 2:
+# Input: digits = [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+# Incrementing by one gives 4321 + 1 = 4322.
+# Thus, the result should be [4,3,2,2].
+
+# Example 3:
+# Input: digits = [9]
+# Output: [1,0]
+# Explanation: The array represents the integer 9.
+# Incrementing by one gives 9 + 1 = 10.
+# Thus, the result should be [1,0].
+ 
+
+def plus_One(num):
+    car = 1
+    for i in range(len(num)-1,-1,-1):
+        sum = (num[i]+car)
+        num[i] = sum % 10
+        car =   sum // 10
+    if car !=0:
+        num.insert(0,car)
+    return num 
+
+digits = [9]
+print(plus_One(digits))
